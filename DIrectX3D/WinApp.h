16f866369e8fd920eb5afc8e268870c14e33325d
Az3r <tuan99kghd@gmail.com>
2020-01-14 22:24:38 +0700

    reuse template

---
+++ DIrectX3D/WinApp.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+class WinApp
+{
+private:
+	HWND m_hwnd;
+	HINSTANCE m_hInstance;
+	WNDCLASSEX m_wc;
+	int m_nCmdShow;
+	std::wstring m_sTitle;
+	std::wstring m_sClassName;
+
+private:
+	static LRESULT CALLBACK SetupWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	WinApp() : m_hwnd(NULL), m_hInstance(NULL), m_wc(), m_nCmdShow(NULL), m_sTitle(L"Desktop Application"), m_sClassName(L"DesktopClass") {}
+	ATOM RegisterWndClassEx();
+
+
+	static void Assert(BOOL condition, wchar_t* msg);
+	static void Assert(HRESULT hr, wchar_t* msg);
+
+	// Asserts Windows API methods' result with error code returned from GetLastError()
+	static void AssertWndFuncs(HRESULT hr);
+	static void AssertWndFuncs(BOOL condition);
+
+public:
+	WinApp(HINSTANCE hInstance, int nCmdShow);
+	~WinApp() {}
+	int InitInstance();
+	int Run();
+	int ExitInstance();
+	static std::wstring GetErrorMsg(DWORD nErrorCode);
+
+};
+
--- DIrectX3D/main.cpp
+++ DIrectX3D/main.cpp
@@ -1,6 +1,12 @@
+#include "WinApp.h"
 #include <Windows.h>
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+    WinApp theApp(hInstance, nCmdShow);
+    theApp.InitInstance();
+    theApp.Run();
+    theApp.ExitInstance();
     return 0;
+
 }
