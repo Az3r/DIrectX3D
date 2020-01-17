8cfe23da24c8c7a12d3c6cdc638026c704531859
Az3r <tuan99kghd@gmail.com>
2020-01-18 01:43:59 +0700

    Update Timer.cpp

--- DIrectX3D/Timer.cpp
+++ DIrectX3D/Timer.cpp
@@ -3,15 +3,15 @@
 
 float Timer::Update() noexcept
 {
-    TimePoint last = mStopWatch;
-    mStopWatch = std::chrono::steady_clock::now();
+    TimePoint last = mCounter;
+    mCounter = std::chrono::steady_clock::now();
 
-    std::chrono::duration<float> offset = std::chrono::duration<float>(mStopWatch - last);
+    std::chrono::duration<float> offset = std::chrono::duration<float>(mCounter - last);
     return offset.count();
 }
 
-float Timer::Peek() const noexcept
+float Timer::Elapsed() const noexcept
 {
-    std::chrono::duration<float> offset = std::chrono::steady_clock::now() - mStopWatch;
+    std::chrono::duration<float> offset = std::chrono::steady_clock::now() - mCounter;
     return offset.count();
 }
