7561aed6f872a31f2cffee1896ba36dfd6212d4a
Az3r <tuan99kghd@gmail.com>
2020-02-04 00:29:28 +0700

    Update Graphics.cpp

--- DIrectX3D/Graphics.cpp
+++ DIrectX3D/Graphics.cpp
@@ -7,6 +7,16 @@ HRESULT Graphics::Initialize()
     DXGI_SWAP_CHAIN_DESC sd;
     ZeroMemory(&sd, sizeof(DXGI_SWAP_CHAIN_DESC));
 
+    sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8X8_UNORM;
+    sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+    sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+    sd.SampleDesc.Count = 1;
+    sd.BufferCount = 1;
+    sd.OutputWindow = mHWND;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    sd.Windowed = TRUE;
+    sd.Flags = NULL;
+
     HRESULT hr = D3D11CreateDeviceAndSwapChain(
         NULL,
         D3D_DRIVER_TYPE_HARDWARE,
@@ -21,9 +31,7 @@ HRESULT Graphics::Initialize()
         NULL,
         &m_pContext
     );
-    try_throw(hr);
 
-
-
+    return hr;
 
 }
